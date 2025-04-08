f714232c57879767deecdaa07608c7e2f238b33c
coolboyalan <deepaky954000@gmail.com>
2025-04-08 15:05:17 +0530

    added cors

--- src/configs/socket.config.js
+++ src/configs/socket.config.js
@@ -25,6 +25,7 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
+  transports: ["websocket", "polling"],
 });
 
 const addUserToList = (userId, socketId) => {
